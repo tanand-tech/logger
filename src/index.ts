aa5b68e54baeb89cb493b3e1c0037c2702119c29
Zhen-dot <65355029+Zhen-dot@users.noreply.github.com>
2021-12-31 12:14:27 +0800

    Fixed webpack: prefix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import autoBind from './auto-bind';
+// @ts-nocheck
 import { Logger, TLogLevelColor, TLogLevelName, ISettingsParam } from 'tslog';
 
 const logLevels: TLogLevelName[] = ['silly', 'trace', 'debug', 'info', 'warn', 'error', 'fatal'];
@@ -30,6 +30,47 @@ let configs: ISettingsParam = {
     },
     logLevelsColors,
 };
+
+const getAllProperties = (object) => {
+    const properties = new Set();
+
+    do {
+        for (const key of Reflect.ownKeys(object)) {
+            properties.add([object, key]);
+        }
+    } while ((object = Reflect.getPrototypeOf(object)) && object !== Object.prototype);
+
+    return properties;
+};
+
+function autoBind(self, { include, exclude } = {}) {
+    const filter = (key) => {
+        const match = (pattern) => (typeof pattern === 'string' ? key === pattern : pattern.test(key));
+
+        if (include) {
+            return include.some(match); // eslint-disable-line unicorn/no-array-callback-reference
+        }
+
+        if (exclude) {
+            return !exclude.some(match); // eslint-disable-line unicorn/no-array-callback-reference
+        }
+
+        return true;
+    };
+
+    for (const [object, key] of getAllProperties(self.constructor.prototype)) {
+        if (key === 'constructor' || !filter(key)) {
+            continue;
+        }
+
+        const descriptor = Reflect.getOwnPropertyDescriptor(object, key);
+        if (descriptor && typeof descriptor.value === 'function') {
+            self[key] = self[key].bind(self);
+        }
+    }
+
+    return self;
+}
 
 export default function logger(name: string, ...args: string[]): Logger {
     const logLevel = process.env.LOGGER_MIN_LEVEL?.toLowerCase() as TLogLevelName | undefined;
